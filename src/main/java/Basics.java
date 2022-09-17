0d3f0e1cec21da2b50dca003dbb1bcfc06519c67
Khai Lam Nguyen <kldtntg@gmail.com>
2022-09-17 19:00:39 -0400

    Exercise 1 1st attempt

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -7,6 +7,8 @@
  * relevant readings for each task.
  */
 
+import java.sql.SQLOutput;
+
 /**
  *  1. In Java, no code can exist outside of a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
@@ -43,7 +45,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,6 +64,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -100,7 +103,9 @@ public class Basics {
          * Current count: 0
          */
 
-
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
     }
 
     /**
@@ -140,11 +145,20 @@ public class Basics {
          *                separated by spaces, so no loops are required, though
          *                you may use them if you wish.
          */
+
+        String[] array = to_split.split(" ", 0);
+
+
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        for (int i=0; i <= 6; i++){
+            ret.append(array[i].charAt(0));
+        }
+        String a;
+        a = ret.toString();
+        return a;
 
-        return ret.toString();
     }
 
     /**
@@ -163,6 +177,7 @@ public class Basics {
      */
     public static int oddSum(int[] arr) {
         int current_sum = 0;
+        if (arr.length < 2){return current_sum;}
 
         /* TODO (Task 5): Complete this method body using a for-loop.
          *                You can find the length of an array by using the
@@ -170,7 +185,11 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-
+        for (int i = 0; i <= arr.length - 1; i++){
+            if (i % 2 != 0){
+                current_sum= current_sum + arr[i];
+            }
+        }
         return current_sum;
     }
 
